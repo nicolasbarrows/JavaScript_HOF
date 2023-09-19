2807b3d8f6a626db7557dbc54e6cb80b4aa3275c
Nicolas Barrows <contact@nicolasbarrows.com>
2023-09-19 08:40:00 -0500

    Exercise 1 COMPLETE

--- app.js
+++ app.js
@@ -2,3 +2,12 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+
+let plus = function (number) {
+  return function (plusNumber) {
+    return plusNumber + number;
+  };
+};
+
+let plus15 = plus(15);
+console.log(plus15(10));
