e102d2e447922ad8c99e26fa4fd40c1678695bfd
Nicolas Barrows <contact@nicolasbarrows.com>
2023-09-19 11:35:36 -0500

    EDIT Exercise 1-6 for clarity/practice

--- app.js
+++ app.js
@@ -1,23 +1,5 @@
 console.log("Hello World!\n==========\n");
 
-// a little excercise title logging function
-let exercise = (number) => {
-  console.log(`EXERCISE ${number}:\n==========\n`);
-};
-
-exercise(1);
-
-let plus = function (number) {
-  return function (plusNumber) {
-    return plusNumber + number;
-  };
-};
-
-let plus15 = plus(15);
-console.log(plus15(10));
-
-exercise(2);
-
 let users = [
   {
     name: "Frodo",
@@ -45,6 +27,24 @@ let users = [
   },
 ];
 
+// a little excercise title logging function
+let exercise = (number) => {
+  console.log(`EXERCISE ${number}:\n==========\n`);
+};
+
+exercise(1);
+
+function plus(num) {
+  return function (plusNum) {
+    return num + plusNum;
+  };
+}
+
+let plus15 = plus(15);
+console.log(plus15(10));
+
+exercise(2);
+
 users.forEach((user) => console.log(user.name));
 
 exercise(3);
@@ -56,25 +56,15 @@ console.log(scoreLog);
 
 exercise(4);
 
-let onlyActive = users.filter((user) => user.isActive == true);
+let onlyActive = users.filter((user) => user.isActive);
 console.log(onlyActive);
 
 exercise(5);
 
-let usersDescending = users.sort(compareScores);
-function compareScores(a, b) {
-  if (a.score > b.score) {
-    return -1;
-  } else if (a.score < b.score) {
-    return 1;
-  } else {
-    return 0;
-  }
-}
-console.log(usersDescending);
+users.sort((a, b) => b.score - a.score);
+console.log(users);
 
 exercise(6);
 
-let scoreTotal = users.reduce((acc,i) => acc + i.score, 0);
-
-console.log(`Total Combined Score: ${scoreTotal}` points);
+let scoreAvg = Math.round(users.reduce((acc, i) => acc + i.score, 0)/users.length);
+console.log(`Average Score: ${scoreAvg} points`);
